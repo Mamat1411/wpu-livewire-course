c116dd9fb1e5fde6797e66df0621f135605b89e6
Mamat1411 <muhammadmujahid14@gmail.com>
2025-09-25 10:24:00 +0700

    Actions

--- app/Livewire/Users.php
+++ app/Livewire/Users.php
@@ -3,12 +3,21 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Livewire\Component;
 
 class Users extends Component
 {
     // public data
     // public $title = 'Users Page';
+
+    public function createUser() {
+        User::create([
+            'name' => fake()->name(),
+            'email' => fake()->email(),
+            'password' => Hash::make('password')
+        ]);
+    }
 
     public function render()
     {
