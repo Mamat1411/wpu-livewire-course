96e26e3f1c3fe957e8aeebe84d1663f8a5e26f7d
Mamat1411 <muhammadmujahid14@gmail.com>
2025-10-06 13:01:56 +0700

    Computed Properties

--- app/Livewire/UsersList.php
+++ app/Livewire/UsersList.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\User;
+use Livewire\Attributes\Computed;
 use Livewire\Attributes\On;
 use Livewire\Component;
 use Livewire\WithPagination;
@@ -29,12 +30,19 @@ class UsersList extends Component
         return view('livewire.placeholders.skeleton');
     }
 
+    // computed properties
+    #[Computed()]
+    public function users() {
+        return User::latest()->where('name', 'like', "%{$this->query}%")->paginate(6);
+    }
+
+    // you can remove this method because the view naming convention is same as livewire component class
     public function render()
     {
         sleep(1);
+
         return view('livewire.users-list', [
-            'title' => 'Users Page',
-            'users' => User::latest()->where('name', 'like', "%{$this->query}%")->paginate(6)
+            'title' => 'Users Page'
         ]);
     }
 }
